ec4fd17596471a535916484fee748b3df0f757d7
Codyvb <37961637+codyvb@users.noreply.github.com>
2025-04-03 17:10:34 -0600

    test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,23 +31,8 @@ export const metadata: Metadata = {
     title: "sidequest.build",
     images: ["./card.png"],
   },
-  // Add Farcaster frame metadata
   other: {
-    "fc:frame": JSON.stringify({
-      version: "next",
-      imageUrl: "https://mini.sidequest.build/fox.png",
-      aspectRatio: "3:2",
-      button: {
-        title: "launch sidequest",
-        action: {
-          type: "launch_frame",
-          name: "launch",
-          url: "https://mini.sidequest.build",
-          splashImageUrl: "https://mini.sidequest.build/card.png",
-          splashBackgroundColor: "#000000"
-        }
-      }
-    })
+    'fc:frame': '{"version":"next","imageUrl":"https://mini.sidequest.build/fox.png","aspectRatio":"3:2","button":{"title":"launch sidequest","action":{"type":"launch_frame","name":"launch","url":"https://mini.sidequest.build","splashImageUrl":"https://mini.sidequest.build/card.png","splashBackgroundColor":"#000000"}}}'
   }
 };
 

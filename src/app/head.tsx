1aef4f2fe0d00638f16086e9ac07788e20f8d2a8
Codyvb <37961637+codyvb@users.noreply.github.com>
2025-04-15 14:30:53 -0600

    mess

--- src/app/head.tsx
+++ src/app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <meta name="fc:frame" content='{"version":"next","imageUrl":"https://mini.sidequest.build/fox.png","aspectRatio":"3:2","button":{"title":"launch sidequest","action":{"type":"launch_frame","name":"launch","url":"https://mini.sidequest.build","splashImageUrl":"https://mini.sidequest.build/card.png","splashBackgroundColor":"#000000"}}}' />
+      <meta name="fc:frame" content='{"version":"next","imageUrl":"https://mini.sidequest.build/goldcard3.png","aspectRatio":"3:2","button":{"title":"launch sidequest","action":{"type":"launch_frame","name":"launch","url":"https://mini.sidequest.build","splashImageUrl":"https://mini.sidequest.build/fox.png","splashBackgroundColor":"#f7f7f7"}}}' />
     </>
   );
 }
